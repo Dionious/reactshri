27cdcb31312f628f504c860f632d6020e1e402b5
Denis <denistimofeev@myrambler.ru>
2024-06-29 14:42:00 +0300

    hide form if not authenticated

--- src/components/filmContent/FilmContent.tsx
+++ src/components/filmContent/FilmContent.tsx
@@ -3,6 +3,8 @@ import styles from './styles/FilmContent.module.css';
 import { Movie } from '../../slices/apiSlice';
 import { useRateMovieMutation } from '../../slices/apiSlice';
 import RatingForm from '../ratingForm/RatingForm';
+import {useSelector} from 'react-redux';
+import {selectAuth} from '../../slices/authSlice';
 
 interface FilmContentProps {
     film: Movie;
@@ -11,6 +13,7 @@ interface FilmContentProps {
 const FilmContent: React.FC<FilmContentProps> = ({ film }) => {
 	const [rateMovie] = useRateMovieMutation();
 	const [userRating, setUserRating] = useState<number | null>(null);
+	const { isAuthenticated } = useSelector(selectAuth);
 
 	useEffect(() => {
 		const storedRating = getLocalMovieRating(film.id);
@@ -50,7 +53,7 @@ const FilmContent: React.FC<FilmContentProps> = ({ film }) => {
 			<div className={styles['film-content']}>
 				<div className={styles['wrapper']}>
 					<h2>{film.title}</h2>
-					<RatingForm onRateMovie={handleRateMovie} currentRating={Number(userRating)} />
+					{isAuthenticated && <RatingForm onRateMovie={handleRateMovie} currentRating={Number(userRating)} /> }
 				</div>
 				<div className={styles['film-content-container']}>
 					<div className={styles['film-content-element']}>
